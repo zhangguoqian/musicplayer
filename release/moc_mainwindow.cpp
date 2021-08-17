73dd0afee6eb2a72a58cf269aa1af099c0691a48
zheaven <1151828901@qq.com>
2021-08-17 14:10:53 +0800

    20210817:添加vscode工作环境

---
+++ release/moc_mainwindow.cpp
@@ -0,0 +1,280 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[38];
+    char stringdata0[705];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 17), // "signalSendIncreas"
+QT_MOC_LITERAL(2, 29, 0), // ""
+QT_MOC_LITERAL(3, 30, 3), // "str"
+QT_MOC_LITERAL(4, 34, 26), // "on_pushButton_Play_clicked"
+QT_MOC_LITERAL(5, 61, 27), // "on_pushButton_Right_clicked"
+QT_MOC_LITERAL(6, 89, 26), // "on_pushButton_Left_clicked"
+QT_MOC_LITERAL(7, 116, 19), // "setIsShowMainWindow"
+QT_MOC_LITERAL(8, 136, 4), // "isOk"
+QT_MOC_LITERAL(9, 141, 21), // "setIsTransparentMouse"
+QT_MOC_LITERAL(10, 163, 23), // "on_activatedSysTrayIcon"
+QT_MOC_LITERAL(11, 187, 33), // "QSystemTrayIcon::ActivationRe..."
+QT_MOC_LITERAL(12, 221, 6), // "reason"
+QT_MOC_LITERAL(13, 228, 25), // "on_pushButton_End_clicked"
+QT_MOC_LITERAL(14, 254, 30), // "on_pushButton_PlayMode_clicked"
+QT_MOC_LITERAL(15, 285, 27), // "on_pushButton_Sound_clicked"
+QT_MOC_LITERAL(16, 313, 26), // "on_pushButton_Word_clicked"
+QT_MOC_LITERAL(17, 340, 27), // "on_pushButton_heart_clicked"
+QT_MOC_LITERAL(18, 368, 15), // "slotItemClicked"
+QT_MOC_LITERAL(19, 384, 11), // "QModelIndex"
+QT_MOC_LITERAL(20, 396, 5), // "index"
+QT_MOC_LITERAL(21, 402, 21), // "slotItemDoubleClicked"
+QT_MOC_LITERAL(22, 424, 38), // "on_horizontalSlider_Sound_val..."
+QT_MOC_LITERAL(23, 463, 5), // "value"
+QT_MOC_LITERAL(24, 469, 17), // "slotMediaPosition"
+QT_MOC_LITERAL(25, 487, 38), // "on_horizontalSlider_Music_val..."
+QT_MOC_LITERAL(26, 526, 17), // "slotSetMediaState"
+QT_MOC_LITERAL(27, 544, 19), // "QMediaPlayer::State"
+QT_MOC_LITERAL(28, 564, 5), // "state"
+QT_MOC_LITERAL(29, 570, 16), // "slotCurrentMedia"
+QT_MOC_LITERAL(30, 587, 8), // "position"
+QT_MOC_LITERAL(31, 596, 20), // "slotLabelWordSetText"
+QT_MOC_LITERAL(32, 617, 14), // "slotMediaError"
+QT_MOC_LITERAL(33, 632, 19), // "QMediaPlayer::Error"
+QT_MOC_LITERAL(34, 652, 5), // "error"
+QT_MOC_LITERAL(35, 658, 11), // "setIncrease"
+QT_MOC_LITERAL(36, 670, 13), // "slotLabelName"
+QT_MOC_LITERAL(37, 684, 20) // "slotUpdatePlayerList"
+
+    },
+    "MainWindow\0signalSendIncreas\0\0str\0"
+    "on_pushButton_Play_clicked\0"
+    "on_pushButton_Right_clicked\0"
+    "on_pushButton_Left_clicked\0"
+    "setIsShowMainWindow\0isOk\0setIsTransparentMouse\0"
+    "on_activatedSysTrayIcon\0"
+    "QSystemTrayIcon::ActivationReason\0"
+    "reason\0on_pushButton_End_clicked\0"
+    "on_pushButton_PlayMode_clicked\0"
+    "on_pushButton_Sound_clicked\0"
+    "on_pushButton_Word_clicked\0"
+    "on_pushButton_heart_clicked\0slotItemClicked\0"
+    "QModelIndex\0index\0slotItemDoubleClicked\0"
+    "on_horizontalSlider_Sound_valueChanged\0"
+    "value\0slotMediaPosition\0"
+    "on_horizontalSlider_Music_valueChanged\0"
+    "slotSetMediaState\0QMediaPlayer::State\0"
+    "state\0slotCurrentMedia\0position\0"
+    "slotLabelWordSetText\0slotMediaError\0"
+    "QMediaPlayer::Error\0error\0setIncrease\0"
+    "slotLabelName\0slotUpdatePlayerList"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      24,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  134,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       4,    0,  137,    2, 0x08 /* Private */,
+       5,    0,  138,    2, 0x08 /* Private */,
+       6,    0,  139,    2, 0x08 /* Private */,
+       7,    1,  140,    2, 0x08 /* Private */,
+       9,    1,  143,    2, 0x08 /* Private */,
+      10,    1,  146,    2, 0x08 /* Private */,
+      13,    0,  149,    2, 0x08 /* Private */,
+      14,    0,  150,    2, 0x08 /* Private */,
+      15,    0,  151,    2, 0x08 /* Private */,
+      16,    0,  152,    2, 0x08 /* Private */,
+      17,    0,  153,    2, 0x08 /* Private */,
+      18,    1,  154,    2, 0x08 /* Private */,
+      21,    1,  157,    2, 0x08 /* Private */,
+      22,    1,  160,    2, 0x08 /* Private */,
+      24,    1,  163,    2, 0x08 /* Private */,
+      25,    1,  166,    2, 0x08 /* Private */,
+      26,    1,  169,    2, 0x08 /* Private */,
+      29,    1,  172,    2, 0x08 /* Private */,
+      31,    0,  175,    2, 0x08 /* Private */,
+      32,    1,  176,    2, 0x08 /* Private */,
+      35,    1,  179,    2, 0x08 /* Private */,
+      36,    0,  182,    2, 0x08 /* Private */,
+      37,    0,  183,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,    8,
+    QMetaType::Void, QMetaType::Bool,    8,
+    QMetaType::Void, 0x80000000 | 11,   12,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 19,   20,
+    QMetaType::Void, 0x80000000 | 19,    2,
+    QMetaType::Void, QMetaType::Int,   23,
+    QMetaType::Void, QMetaType::LongLong,   23,
+    QMetaType::Void, QMetaType::Int,   23,
+    QMetaType::Void, 0x80000000 | 27,   28,
+    QMetaType::Void, QMetaType::Int,   30,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 33,   34,
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->signalSendIncreas((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 1: _t->on_pushButton_Play_clicked(); break;
+        case 2: _t->on_pushButton_Right_clicked(); break;
+        case 3: _t->on_pushButton_Left_clicked(); break;
+        case 4: _t->setIsShowMainWindow((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 5: _t->setIsTransparentMouse((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 6: _t->on_activatedSysTrayIcon((*reinterpret_cast< QSystemTrayIcon::ActivationReason(*)>(_a[1]))); break;
+        case 7: _t->on_pushButton_End_clicked(); break;
+        case 8: _t->on_pushButton_PlayMode_clicked(); break;
+        case 9: _t->on_pushButton_Sound_clicked(); break;
+        case 10: _t->on_pushButton_Word_clicked(); break;
+        case 11: _t->on_pushButton_heart_clicked(); break;
+        case 12: _t->slotItemClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 13: _t->slotItemDoubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 14: _t->on_horizontalSlider_Sound_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 15: _t->slotMediaPosition((*reinterpret_cast< qint64(*)>(_a[1]))); break;
+        case 16: _t->on_horizontalSlider_Music_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 17: _t->slotSetMediaState((*reinterpret_cast< QMediaPlayer::State(*)>(_a[1]))); break;
+        case 18: _t->slotCurrentMedia((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 19: _t->slotLabelWordSetText(); break;
+        case 20: _t->slotMediaError((*reinterpret_cast< QMediaPlayer::Error(*)>(_a[1]))); break;
+        case 21: _t->setIncrease((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 22: _t->slotLabelName(); break;
+        case 23: _t->slotUpdatePlayerList(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 17:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaPlayer::State >(); break;
+            }
+            break;
+        case 20:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaPlayer::Error >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (MainWindow::*)(QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::signalSendIncreas)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 24)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 24;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 24)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 24;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MainWindow::signalSendIncreas(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
